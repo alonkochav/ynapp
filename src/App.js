d2687c0cf4048b9bfc2115fa4c632fa01a3f16aa
Alon Kochav <alonkochav@gmail.com>
2019-03-04 16:27:00 +0200

    ynatest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import items from './items.json';
+
 
 class App extends Component {
   
 
   handleDelete(e){
     console.log(e.target.ref);
+  }
+
+  getItemsFromJson(){
+    const listItems = items.map((item) =>
+    <li ref={item.id}><input type="checkbox" name='item' id={item.id} /><label>{item.name}</label><span ref={item.id} onClick={this.handleDelete}>x</span> </li> 
+
+    );
+    return listItems;
+  }
+
+  addItem(e){
+    alert(e.target.value);
   }
   
   render() {
@@ -18,21 +32,18 @@ class App extends Component {
           </h1>
         </header>
 
-        <div class="App-body">
-          <div class="App-body-items">
-          <h2 class="App-body-header title">
-            This is your list
+        <div className="App-body">
+          <div className="App-body-items">
+          <h2 className="App-body-header title">
+             Add your items here
           </h2>
           <ul>
-            <li ref><input type="checkbox" name='item' /><label>Bread</label><span ref="1" onClick={this.handleDelete}>x</span> </li>
-            <li ref><input type="checkbox" name='item' /><label>Milk</label><span ref="2" onClick={this.handleDelete}>x</span> </li>
-            <li ref><input type="checkbox" name='item' /><label>Cheese</label><span ref="3" onClick={this.handleDelete}>x</span> </li>
-            <li ref><input type="checkbox" name='item' /><label>Cheese</label><span value="4" onClick={this.handleDelete}>x</span> </li>
+            { this.getItemsFromJson() }
             <li ref><input type="text" name='additem' placeholder="Add a new item" /><span value="4" onClick={this.handleDelete}>x</span> </li>
-
+            <form name="addItem" onSubmit={this.addItem}></form>
+            <input type="submit" hidden/>
           </ul>
 
-          
               </div>
 
         
